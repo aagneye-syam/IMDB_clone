4849126f6c7bbca0374d281348473755651e4eab
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 20:24:04 +0530

    card.jsx does not match the corresponding name on disk err resolved

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react"
 import "./MovieList.css"
 import { useParams } from "react-router-dom"
-import Cards from "../card/card"
+import Cards from "../card/Card"
 
 const MovieList = () => {
     
