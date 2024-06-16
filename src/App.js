914c08587b28b47813bc41215158e4ee84277fdd
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 20:44:01 +0530

    some err resolved

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import MovieList from "./components/movieList/MovieList";
+import Movie from "./pages/movieDetails/Movie";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route index element={<Home />} />
-          <Route path="movie/:id" element={<h1>movie detail</h1>}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
           <Route path="movies/:type" element={<MovieList />}></Route>
           <Route path="/*" element={<h1>Error page</h1>}></Route>
         </Routes>
