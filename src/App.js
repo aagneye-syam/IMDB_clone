acbcb87c430a0d5146ccf8ce1c804cb0bf73e5b8
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 15:32:56 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route index element={<h1>Hello World</h1>}/>
+        
+                <Route path="movie/:id" element={<h1>movie detail</h1>}></Route>
+                <Route path="movies/:type" element={<h1>movies lists</h1>}></Route>
+                <Route path="/*" element={<h1>Error page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
