50491ef557e6d7f2f3fa5e8e5bcff03eaa030c48
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 15:48:47 +0530

    Header edited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route index element={<h1>Hello World</h1>}/>
         
