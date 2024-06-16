3c3434f9661544eb19aa7fe0b76da2601f176ac7
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 23:02:03 +0530

    some styling added to SignUp form

--- src/pages/auth/auth.jsx
+++ src/pages/auth/auth.jsx
@@ -1,30 +1,45 @@
-
 import { useNavigate } from "react-router-dom";
-import './auth.css'
+import "./auth.css";
 
 export const Auth = () => {
-
   const navigate = useNavigate();
- 
+
   const handleLogin = () => {
     navigate("/");
-  }
+  };
   return (
     <div class="container">
-    <form action="/" method="post">
-      <label for="username">Email or Phone Number:</label>
-      <input type="text" id="username" name="username" placeholder="Enter email or phone number" />
+      <form action="/" method="post">
+        <label for="username">Email or Phone Number:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Enter email or phone number"
+        />
 
-      <label for="password">Password:</label>
-      <div class="password-container">
-        <input type="password" id="password" name="password" placeholder="Password" />
-        <i class="fas fa-eye" id="togglePassword"></i> </div>
+        <label for="password">Password:</label>
+        <div class="password-container">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+          />
+          <i class="fas fa-eye" id="togglePassword"></i>{" "}
+        </div>
 
-      <button onClick={handleLogin}>Log In</button>
+        <button onClick={handleLogin}>Log In</button>
 
-      <a href="#" class="forgot-password">Forgot Password?</a>
-      <p>Don't have an account? <a href="#">Sign Up</a></p>
-    </form>
-  </div>
+        <div className="forget_password">
+          <a href="#" class="forgot-password">
+            Forgot Password?
+          </a>
+        </div>
+        <p>
+          Don't have an account? <a href="#">Sign Up</a>
+        </p>
+      </form>
+    </div>
   );
 };
