a34bd5aeac07b602b513a80023828d0a5fb11ecd
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 15:54:50 +0530

    logo image added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import "./Header.css"
 import { Link } from "react-router-dom"
-import {logoImage} from './assets/logo.png'
+import logoImage from './assets/logo.png'
 
 const Header = () => {
     return (
