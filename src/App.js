c7d2295282985d417692027057c23950216b4d70
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 16:15:15 +0530

    Home page def and styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Header from './components/header/Header';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -8,11 +9,10 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route index element={<h1>Hello World</h1>}/>
-        
-                <Route path="movie/:id" element={<h1>movie detail</h1>}></Route>
-                <Route path="movies/:type" element={<h1>movies lists</h1>}></Route>
-                <Route path="/*" element={<h1>Error page</h1>}></Route>
+          <Route index element={<Home />} />
+          <Route path="movie/:id" element={<h1>movie detail</h1>}></Route>
+          <Route path="movies/:type" element={<h1>movies lists</h1>}></Route>
+          <Route path="/*" element={<h1>Error page</h1>}></Route>
         </Routes>
       </Router>
     </div>
